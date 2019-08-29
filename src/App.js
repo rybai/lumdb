27f0f5f9f5735c5968429cc3b2d8d599f68a51e4
rybai <rybai86@gmail.com>
2019-08-29 10:10:49 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  },
+  {
+    id: 2,
+    title: 'Spider Man'
+  },
+  {
+    id: 3,
+    title: 'The Avengers'
+  }
+];
+
 function App() {
+  // // const [toggle, setToggle] = useState(true);
+  // const [input, setInput] = useState('Hello');
+
+  // const submit = () => {
+  //   console.log(textInput.current.value);
+  // };
+
+  // const handleTextChange = event => {
+  //   setInput(event.target.value.trim());
+  // };
+  // const textInput = useRef(); // Uncontrolled input - no validation
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+function Welcome({ text }) {
+  return (
+    <div>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {text}
+      </a>
     </div>
   );
 }
